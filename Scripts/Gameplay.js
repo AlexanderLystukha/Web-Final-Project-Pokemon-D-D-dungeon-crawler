de8ada02bd718e9ec9f9578758c130c93f1c7ffe
Alexander Lystukha <6245430@johnabbottcollege.net>
2024-12-24 02:12:14 -0500

    battle interface created

--- Scripts/Gameplay.js
+++ Scripts/Gameplay.js
@@ -1,12 +1,11 @@
 "use strict";
 
-$(document).ready(function () {
-  //Gets all the images
-  const player = JSON.parse(localStorage.getItem("player"));
-  const character = JSON.parse(localStorage.getItem("character"));
+let inventory = {}
 
-  PopulatePlayerInfo(player, character);
-});
+const player = JSON.parse(localStorage.getItem("player"));
+const character = JSON.parse(localStorage.getItem("character"));
+
+const dialogueBox = document.getElementById("gameText") 
 
 function PopulatePlayerInfo(player, character) {
   const playerImage = document.getElementById(`playerImage`);
@@ -22,3 +21,36 @@ function PopulatePlayerInfo(player, character) {
   console.log(newImage);
   playerImage.src = newImage;
 }
+
+function showTextDialogue(dialogueIndex){
+  const dialogue = TextDialog.find(dialogue => dialogue.id === dialogueIndex)
+  dialogueBox.textContent = dialogue.text
+  while()
+}
+
+function StartGame(){
+
+}
+
+const TextDialog = [
+  {
+    id: 1,
+    text: "test",
+    options: [
+      {
+        text: "do your mom",
+        nextText: 2
+      },
+      {
+        text: "kisses",
+        nextText: 2
+      }
+    ]
+  },
+]
+
+PopulatePlayerInfo(player, character);
+
+let pageCount = 0;
+
+
